4fc2e059fa7e7106763ea417b7dcc1fa2ce37365
satoshi <yamatomi3104@gmail.com>
2025-07-31 23:14:26 +0900

    firebase setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,16 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui',"@nuxtjs/tailwindcss"],
   css: ["@/assets/css/common.scss"],
-
+   runtimeConfig: {
+    public: {
+      ENV: process.env[process.env.NODE_ENV + "_" + "ENV"],
+      apiKey: process.env[process.env.NODE_ENV + "_" + "apiKey"],
+      authDomain: process.env[process.env.NODE_ENV + "_" + "authDomain"],
+      projectId: process.env[process.env.NODE_ENV + "_" + "projectId"],
+      storageBucket: process.env[process.env.NODE_ENV + "_" + "storageBucket"],
+      databaseURL: process.env[process.env.NODE_ENV + "_" + "databaseURL"],
+      SENTRY_KEY: process.env.SENTRY_KEY,
+      lineAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    },
+  },
 })
